09e694c0827ab8b7e7faeac858032dbd4ec5dfed
enzomolloy <enzomolloy@gmail.com>
2021-10-27 19:42:47 -0300

    Cambiado 2.0

--- MenuVerduleria.Designer.cs
+++ MenuVerduleria.Designer.cs
@@ -33,6 +33,8 @@ namespace TPVerduleria
             this.lbVerduraFavorita = new System.Windows.Forms.Label();
             this.btnCambiarVerdura = new System.Windows.Forms.Button();
             this.btnCerrar = new System.Windows.Forms.Button();
+            this.cbxColores = new System.Windows.Forms.ComboBox();
+            this.cbxVerduras = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // lbNombreUsuario
@@ -40,7 +42,7 @@ namespace TPVerduleria
             this.lbNombreUsuario.AutoSize = true;
             this.lbNombreUsuario.BackColor = System.Drawing.Color.Transparent;
             this.lbNombreUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNombreUsuario.Location = new System.Drawing.Point(525, 176);
+            this.lbNombreUsuario.Location = new System.Drawing.Point(481, 178);
             this.lbNombreUsuario.Name = "lbNombreUsuario";
             this.lbNombreUsuario.Size = new System.Drawing.Size(79, 29);
             this.lbNombreUsuario.TabIndex = 0;
@@ -51,7 +53,7 @@ namespace TPVerduleria
             this.lbVerduraFavorita.AutoSize = true;
             this.lbVerduraFavorita.BackColor = System.Drawing.Color.Transparent;
             this.lbVerduraFavorita.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbVerduraFavorita.Location = new System.Drawing.Point(525, 285);
+            this.lbVerduraFavorita.Location = new System.Drawing.Point(481, 285);
             this.lbVerduraFavorita.Name = "lbVerduraFavorita";
             this.lbVerduraFavorita.Size = new System.Drawing.Size(79, 29);
             this.lbVerduraFavorita.TabIndex = 1;
@@ -61,7 +63,7 @@ namespace TPVerduleria
             // 
             this.btnCambiarVerdura.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnCambiarVerdura.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCambiarVerdura.Location = new System.Drawing.Point(586, 475);
+            this.btnCambiarVerdura.Location = new System.Drawing.Point(473, 471);
             this.btnCambiarVerdura.Name = "btnCambiarVerdura";
             this.btnCambiarVerdura.Size = new System.Drawing.Size(131, 53);
             this.btnCambiarVerdura.TabIndex = 2;
@@ -81,12 +83,35 @@ namespace TPVerduleria
             this.btnCerrar.UseVisualStyleBackColor = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
+            // cbxColores
+            // 
+            this.cbxColores.FormattingEnabled = true;
+            this.cbxColores.Items.AddRange(new object[] {
+            "Rojo",
+            "Verde",
+            "Amarillo"});
+            this.cbxColores.Location = new System.Drawing.Point(755, 351);
+            this.cbxColores.Name = "cbxColores";
+            this.cbxColores.Size = new System.Drawing.Size(121, 21);
+            this.cbxColores.TabIndex = 4;
+            this.cbxColores.SelectedIndexChanged += new System.EventHandler(this.cbxColores_SelectedIndexChanged);
+            // 
+            // cbxVerduras
+            // 
+            this.cbxVerduras.FormattingEnabled = true;
+            this.cbxVerduras.Location = new System.Drawing.Point(755, 417);
+            this.cbxVerduras.Name = "cbxVerduras";
+            this.cbxVerduras.Size = new System.Drawing.Size(121, 21);
+            this.cbxVerduras.TabIndex = 5;
+            // 
             // MenuVerduleria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::TPVerduleria.Properties.Resources.Group_3;
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.cbxVerduras);
+            this.Controls.Add(this.cbxColores);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.btnCambiarVerdura);
             this.Controls.Add(this.lbVerduraFavorita);
@@ -105,5 +130,7 @@ namespace TPVerduleria
         private System.Windows.Forms.Label lbVerduraFavorita;
         private System.Windows.Forms.Button btnCambiarVerdura;
         private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.ComboBox cbxColores;
+        private System.Windows.Forms.ComboBox cbxVerduras;
     }
 }
--- MenuVerduleria.cs
+++ MenuVerduleria.cs
@@ -37,19 +37,65 @@ namespace TPVerduleria
             string ComidaFav = reader["Nombre"].ToString();
 
             lbVerduraFavorita.Text = "Comida Favorita: " + ComidaFav;
+
+            connection.Close();
+            
+        }
+
+
+        private void btnCambiarVerdura_Click(object sender, EventArgs e)
+        {
+
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+
+            command.CommandText = "select * from Comida where Nombre='" + cbxVerduras.Text + "'";
+            OleDbDataReader reader = command.ExecuteReader();
+            reader.Read();
+
+            string IDVeruda = reader["ID"].ToString();
+
+            connection.Close();
+            connection.Open();
+
+            command.CommandText = "update Usuario set ComidaFavorita='" + IDVeruda + "' where Usuario='" + IniciarSesion.ObtenerDatosUsuario.NombreDelUsuario + "'";
+            command.ExecuteNonQuery();
+
+            MessageBox.Show("Tu Comida Se Cambio!");
+
+            connection.Close();
+        }
+
+        private void cbxColores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxVerduras.Items.Clear();
+            cbxVerduras.Text = "";
+
+            if (cbxColores.Text == "Verde")
+            {
+                cbxVerduras.Items.Add("Pera");
+                cbxVerduras.Items.Add("Palta");
+                cbxVerduras.Items.Add("Lechuga");
+            }
+            else if (cbxColores.Text == "Rojo")
+            {
+                cbxVerduras.Items.Add("Tomate");
+                cbxVerduras.Items.Add("Frutilla");
+                cbxVerduras.Items.Add("Cerezas");
+            }
+            else if (cbxColores.Text == "Amarillo")
+            {
+                cbxVerduras.Items.Add("Limon");
+                cbxVerduras.Items.Add("Banana");
+                cbxVerduras.Items.Add("Melon");
+            }
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             this.Hide();
             new IniciarSesion().ShowDialog();
-            this.Show();
-        }
-
-        private void btnCambiarVerdura_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            new CambioDeVerdura().ShowDialog();
             this.Show();
         }
     }
